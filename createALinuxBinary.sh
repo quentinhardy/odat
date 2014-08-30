23a74ea0e124a468478f8cb211a6d5c53eb5b996
Quentin HARDY <qhardyfr@gmail.com>
2014-08-30 18:44:48 +0200

    Update the createALinuxBinary.sh file to fix bug when there is not built/linux folders

--- createALinuxBinary.sh
+++ createALinuxBinary.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-
 GLIBC_VERSION=`ldd --version | grep ldd | cut -d ' ' -f 5`
 VERSION="libc$GLIBC_VERSION-`uname -m`"
 echo $NAME
 #Creation
+mkdir -p ./build/linux/
 pyinstaller --clean --onedir --noconfirm --distpath="./build/linux/" --workpath="./build/" --name="odat-$VERSION" odat.py --additional-hooks-dir='/usr/lib/python2.7/dist-packages/scapy/layers/' --strip
 #Add a librarie manually
 cp "$ORACLE_HOME"/lib/libociei.so ./build/linux/odat-$VERSION/libociei.so
@@ -23,5 +23,6 @@ read -p "Do you want delete no compressed data (Y or y for yes)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
- rm -r ./odat-$VERSION/
+rm -r ./odat-$VERSION/
 fi
+

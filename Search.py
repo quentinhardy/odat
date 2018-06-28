f8cf5248b4e49816943e50506f315965a92a89fa
quentinhardy <quentin.hardy@protonmail.com>
2018-06-28 10:23:07 +0200

    option for desc each tables and --without-example option

--- Search.py
+++ Search.py
@@ -14,6 +14,8 @@ class Search (OracleDatabase):
 	
 	REQ_INFO_FROM_COLUMN_NAMES = "SELECT owner, table_name, column_name FROM all_tab_columns WHERE column_name LIKE '{0}'" #{0}==pattern
 	REQ_VALUE_IN_COLUMN = 'SELECT "{0}" FROM "{1}"."{2}" WHERE "{0}" is not null and rownum = 1' #{0}==column, {1}==database, {2}==table
+	REQ_GET_ALL_NO_SYSTEM_TABLES = "SELECT DISTINCT owner, table_name FROM all_tables WHERE owner not in ('SYS','SYSTEM')"
+	REQ_GET_COLUMNS_FOR_TABLE = "SELECT column_name, data_type FROM all_tab_columns WHERE table_name='{0}' and owner='{1}'" #{0}==table name, {1}==owner
 	DEFAULT_VALUE_EMPTY_COLUMN = "(Empty Column)"
 	DEFAULT_VALUE_UNKNOWN = "(Unknown)"
 	EXEMPLE_VALUE_LEN_MAX = 40
@@ -26,24 +28,24 @@ class Search (OracleDatabase):
 		logging.debug("Search object created")
 		OracleDatabase.__init__(self,args)
 
-	def searchInColumns(self, sqlPattern, showEmptyColumns):
+	def searchInColumns(self, sqlPattern, showEmptyColumns, withoutExample=False):
 		'''
 		Search sqlpattern in all columns names
 		returns a list which contains dicos ex: [{'columnName': 'passwd', 'tableName':'users', 'database':'mysite'}{'columnName': 'password', 'tableName':'users', 'database':'mysitetemp'}]
 		'''
 		logging.info("Searching pattern '{0}' in column names".format(sqlPattern.upper()))
 		results = self.__execQuery__(query=self.REQ_INFO_FROM_COLUMN_NAMES.format(sqlPattern.upper()), ld=['owner', 'table_name', 'column_name'])
-		table = self.getInfoIntable(results, ["owner","table_name","column_name", "example"], showEmptyColumns=showEmptyColumns)
+		table = self.getInfoIntable(results, ["owner","table_name","column_name", "example"], showEmptyColumns=showEmptyColumns, withoutExample=withoutExample)
 		return table
 		
-	def searchPwdKeyworkInColumnNames(self, showEmptyColumns):
+	def searchPwdKeyworkInColumnNames(self, showEmptyColumns, withoutExample=False):
 		'''
 		Search sqlpattern in all columns names
 		returns a list which contains dicos ex: [{'columnName': 'passwd', 'tableName':'users', 'database':'mysite'}{'columnName': 'password', 'tableName':'users', 'database':'mysitetemp'}]
 		'''
 		tables = ""
 		for aPattern in PATTERNS_COLUMNS_WITH_PWDS:
-			table = self.searchInColumns(aPattern, showEmptyColumns=showEmptyColumns)
+			table = self.searchInColumns(aPattern, showEmptyColumns=showEmptyColumns, withoutExample=withoutExample)
 			if self.isEmptyTable(table) == True :
 				logging.debug("Nothing to print. Doesn't print the header of the table!")
 			else : 
@@ -54,7 +56,42 @@ class Search (OracleDatabase):
 					print "------->"+e
 		return tables
 		
-	def getInfoIntable(self,listOfDicos, columns, showEmptyColumns):
+	def getDescOfEachNoSystemTable(self):
+		'''
+		returns a String for print
+		'''
+		outputString = ""
+		logging.debug("Getting all no system tables accessible with the current user")
+		tablesAccessible = self.__execQuery__(query=self.REQ_GET_ALL_NO_SYSTEM_TABLES, ld=['owner', 'table_name'])
+		if isinstance(tablesAccessible,Exception):
+			logging.warning("Impossible to execute the request '{0}': {1}".format(self.REQ_GET_ALL_NO_SYSTEM_TABLES, tablesAccessible.generateInfoAboutError(self.REQ_GET_ALL_NO_SYSTEM_TABLES)))
+			return ""
+		else:
+			nbTables = len(tablesAccessible)
+			colNb = nbTables
+			if colNb>0 : 
+				pbar,currentColNum = self.getStandardBarStarted(colNb), 0
+			for aTable in tablesAccessible:
+				if colNb>0:
+					currentColNum += 1
+					pbar.update(currentColNum)
+				request = self.REQ_GET_COLUMNS_FOR_TABLE.format(aTabl<e['table_name'], aTable['owner'])
+				columnsAndTypes = self.__execQuery__(query=request, ld=['column_name', 'data_type'])
+				if isinstance(columnsAndTypes,Exception):
+					logging.warning("Impossible to execute the request '{0}': {1}".format(request, columnsAndTypes.generateInfoAboutError(request)))
+				outputString += "\n[+] {0}.{1} ({2}/{3})\n".format(aTable['owner'], aTable['table_name'], currentColNum, colNb)
+				resultsToTable = [('column_name', 'data_type')]
+				for aLine in columnsAndTypes:
+					resultsToTable.append((aLine['column_name'], aLine['data_type']))
+				table = Texttable(max_width=getScreenSize()[0])
+				table.set_deco(Texttable.HEADER)
+				table.add_rows(resultsToTable)
+				outputString += table.draw()
+				outputString += '\n'
+			if colNb>0 : pbar.finish()
+		return outputString
+		
+	def getInfoIntable(self,listOfDicos, columns, showEmptyColumns, withoutExample=False):
 		'''
 		columns: list which contains column names for the output
 		returns a String for print
@@ -71,25 +108,29 @@ class Search (OracleDatabase):
 			l.append(e['owner'])
 			l.append(e['table_name'])
 			l.append(e['column_name'])
-			logging.debug("Search a not null value in the column '{0}' of the table '{1}'.'{2}' ({3}/{4})".format(e['column_name'], e['owner'], e['table_name'],currentColNum,colNb))
-			req_value_in_column = self.REQ_VALUE_IN_COLUMN.format(e['column_name'],e['owner'],e['table_name'])
-			aValue = self.__execQuery__(query=req_value_in_column, ld=['value'])
-			if isinstance(aValue,Exception):
-				logging.warning("Impossible to execute the request '{0}' in column names: {1}".format(req_value_in_column, aValue.generateInfoAboutError(req_value_in_column)))
+			if withoutExample == True:
 				l.append(self.DEFAULT_VALUE_UNKNOWN)
-			elif aValue == [] : l.append(self.DEFAULT_VALUE_EMPTY_COLUMN)
-			else :
-				value = aValue[0]['value']
-				if type(value) is str: 
-					isStringValueInColumn = True
-					if len(value) > self.EXEMPLE_VALUE_LEN_MAX:
-						value = value[0:self.EXEMPLE_VALUE_LEN_MAX] + ' ' +self.TRUNCATED_MESSAGE_EXEMPLE
-					l.append(value)
-				else: l.append(self.DEFAULT_VALUE_EMPTY_COLUMN)
-			if isStringValueInColumn == True :
 				resultsToTable.append(l)
-			elif showEmptyColumns==True :
-				resultsToTable.append(l)
+			else:
+				logging.debug("Search a not null value in the column '{0}' of the table '{1}'.'{2}' ({3}/{4})".format(e['column_name'], e['owner'], e['table_name'],currentColNum,colNb))
+				req_value_in_column = self.REQ_VALUE_IN_COLUMN.format(e['column_name'],e['owner'],e['table_name'])
+				aValue = self.__execQuery__(query=req_value_in_column, ld=['value'])
+				if isinstance(aValue,Exception):
+					logging.warning("Impossible to execute the request '{0}' in column names: {1}".format(req_value_in_column, aValue.generateInfoAboutError(req_value_in_column)))
+					l.append(self.DEFAULT_VALUE_UNKNOWN)
+				elif aValue == [] : l.append(self.DEFAULT_VALUE_EMPTY_COLUMN)
+				else :
+					value = aValue[0]['value']
+					if type(value) is str: 
+						isStringValueInColumn = True
+						if len(value) > self.EXEMPLE_VALUE_LEN_MAX:
+							value = value[0:self.EXEMPLE_VALUE_LEN_MAX] + ' ' +self.TRUNCATED_MESSAGE_EXEMPLE
+						l.append(value)
+					else: l.append(self.DEFAULT_VALUE_EMPTY_COLUMN)
+				if isStringValueInColumn == True :
+					resultsToTable.append(l)
+				elif showEmptyColumns==True :
+					resultsToTable.append(l)
 		if colNb>0 : pbar.finish()
 		table = Texttable(max_width=getScreenSize()[0])
 		table.set_deco(Texttable.HEADER)
@@ -119,7 +160,7 @@ def runSearchModule(args):
 	Run the Search module
 	'''
 	status = True
-	if checkOptionsGivenByTheUser(args,["test-module","column-names","pwd-column-names"]) == False : return EXIT_MISS_ARGUMENT
+	if checkOptionsGivenByTheUser(args,["test-module","column-names","pwd-column-names","desc-tables","without-example"]) == False : return EXIT_MISS_ARGUMENT
 	search = Search(args)
 	status = search.connection(stopIfError=True)
 	if args['test-module'] == True :
@@ -127,15 +168,19 @@ def runSearchModule(args):
 		status = search.testAll()
 	if args.has_key('column-names')==True and args['column-names']!=None:
 		args['print'].title("Columns which contains the pattern '{0}'".format(args['column-names']))
-		table = search.searchInColumns(args['column-names'],showEmptyColumns=args['show-empty-columns'])
+		table = search.searchInColumns(args['column-names'],showEmptyColumns=args['show-empty-columns'], withoutExample=args['without-example'])
 		if search.isEmptyTable(table) == True :
 			args['print'].badNews("no result found")
 		else :
 			args['print'].goodNews(table)
 	if args['pwd-column-names']==True:
 		args['print'].title("Columns which contains the pattern ~password~ like (multi language)")
-		table = search.searchPwdKeyworkInColumnNames(showEmptyColumns=args['show-empty-columns'])
+		table = search.searchPwdKeyworkInColumnNames(showEmptyColumns=args['show-empty-columns'], withoutExample=args['without-example'])
 		if search.isEmptyTable(table) == True :
                         args['print'].badNews("no result found")
                 else :
                         args['print'].goodNews(table)
+	if args['desc-tables']==True:
+		args['print'].title("Descibe each table which is accessible by the current user (without system tables)")
+		table = search.getDescOfEachNoSystemTable()
+		print table

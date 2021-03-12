391060df71dcf844c08f4b6525e98bb449b2daeb
quentinhardy <quentin.hardy@protonmail.com>
2021-03-12 10:13:39 +0100

    Update version to 5.1

--- Constants.py
+++ Constants.py
@@ -16,7 +16,7 @@ DESCRIPTION = ""\
 
 By Quentin Hardy (quentin.hardy@protonmail.com or quentin.hardy@bt.com)
 """
-CURRENT_VERSION = "Version 5.0 - 2021/02/08"
+CURRENT_VERSION = "Version 5.1 - 2021/03/12"
 DEFAULT_SID_MIN_SIZE = 1
 DEFAULT_SID_MAX_SIZE = 2
 MAX_HELP_POSITION=22
